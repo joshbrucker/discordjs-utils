d64f382d673e82e4bf3a6111ea3c84323fe9cb10
Joshua Brucker <joshbrucker@gmail.com>
2025-07-27 21:24:13 -0400

    Export custom PagedEmbedSendError

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { PagedEmbed } from "./paging/PagedEmbed.js";
+export { PagedEmbed, PagedEmbedSendError } from "./paging/PagedEmbed.js";
 export { PagedEmbedOptions } from "./paging/PagedEmbedOptions.js";
 export { ignore } from "./utils/errorHandlers.js";
 export * as emojiUtils from "./utils/emojiUtils.js";
